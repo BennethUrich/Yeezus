5dfe8417f16a09816f3c77936e858158666ea747
BennethUrich <bennethurich18@gmail.com>
2022-09-24 00:48:20 -0300

    apYeezus

---
+++ apYeezus/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace apYeezus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.ofdAbrir = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSalvar = new System.Windows.Forms.SaveFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lstPlaylist = new System.Windows.Forms.ListBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.Color.Red;
+            this.btnAbrir.Location = new System.Drawing.Point(12, 32);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(63, 34);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.Red;
+            this.btnSalvar.Location = new System.Drawing.Point(12, 74);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(63, 34);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.BackColor = System.Drawing.Color.Red;
+            this.btnCarregar.Location = new System.Drawing.Point(11, 116);
+            this.btnCarregar.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(63, 34);
+            this.btnCarregar.TabIndex = 4;
+            this.btnCarregar.UseVisualStyleBackColor = false;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Red;
+            this.btnLimpar.Location = new System.Drawing.Point(12, 158);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(63, 34);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "yeezusLogoOfc.png");
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(287, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(473, 138);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lstPlaylist
+            // 
+            this.lstPlaylist.BackColor = System.Drawing.Color.Black;
+            this.lstPlaylist.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstPlaylist.Font = new System.Drawing.Font("Onyx", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPlaylist.ForeColor = System.Drawing.Color.White;
+            this.lstPlaylist.FormattingEnabled = true;
+            this.lstPlaylist.ItemHeight = 27;
+            this.lstPlaylist.Location = new System.Drawing.Point(399, 120);
+            this.lstPlaylist.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.lstPlaylist.Name = "lstPlaylist";
+            this.lstPlaylist.Size = new System.Drawing.Size(246, 189);
+            this.lstPlaylist.TabIndex = 10;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(241, 251);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 243);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(27, 108);
+            this.player.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(264, 193);
+            this.player.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(753, 318);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.lstPlaylist);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnAbrir);
+            this.Font = new System.Drawing.Font("Onyx", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YEEZUS.EXE";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.OpenFileDialog ofdAbrir;
+        private System.Windows.Forms.SaveFileDialog sfdSalvar;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox lstPlaylist;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private AxWMPLib.AxWindowsMediaPlayer player;
+    }
+}
+
---
+++ apYeezus/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace apYeezus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WMPLib.IWMPPlaylist playList;
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            ofdAbrir.Title = "Abrir midia";
+            ofdAbrir.Filter = "Arquivo mp4|*.mp4|Arquivo mp3|*.mp3|Arquivo MPEG-4|*.m4a"; //filtrar para exibir apenas arquivos de audio e video
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                playList = player.playlistCollection.newPlaylist("lista");
+
+                foreach (var arquivo in ofdAbrir.FileNames)
+                {
+                    playList.appendItem(player.newMedia(arquivo));
+                    lstPlaylist.Items.Add(arquivo);
+
+                    player.currentPlaylist = playList;
+                    player.Ctlcontrols.play();
+                }
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if(lstPlaylist.Items.Count > 0)
+            {
+                sfdSalvar.Title = "Salvar Playlist";
+                sfdSalvar.Filter = "Arquivo texto|*.txt";
+                if(sfdSalvar.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter arquivo = new StreamWriter(sfdSalvar.FileName, false);
+                    for (int indice = 0; indice < lstPlaylist.Items.Count -1; indice++)
+                    {
+                        arquivo.WriteLine(lstPlaylist.Items[indice].ToString());
+                    }
+                    arquivo.Close();
+                }
+            }
+
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+            sfdSalvar.Title = "Abrir Playlist";
+            sfdSalvar.Filter = "Arquivo texto|*.txt";
+            ofdAbrir.Multiselect = false;
+            if (ofdAbrir.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader arquivo = new StreamReader(ofdAbrir.FileName);
+                while (arquivo.Peek() != -1)
+                {
+                    lstPlaylist.Items.Add(arquivo.ReadLine());
+                }
+                arquivo.Close();
+            }
+        }
+
+        private void lstPlaylist_DoubleClick(object sender, EventArgs e)
+        {
+            if(lstPlaylist.Items.Count > 0)
+            {
+                player.URL = lstPlaylist.SelectedItem.ToString();
+                player.Ctlcontrols.play();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.stop();
+            lstPlaylist.Items.Clear();
+        }
+    }
+}
